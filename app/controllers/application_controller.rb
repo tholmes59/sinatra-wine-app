31f565834281ad57b3441254ea617c9b84141218
Tom Holmes <tholmes59@gmail.com>
2019-04-15 20:08:05 +0000

    add helper methods logged_in? and current user and improved get method to redirect to show page if logged in in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,23 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/" do
-    erb :index
+    if logged_in?
+      redirect "/show/#{current_user.id}"
+    else 
+      erb :index
+    end
   end
+  
+  helpers do
+    
+		def logged_in?
+			!!current_user
+		end
+
+		def current_user
+			@current_user ||= User.find(session[:user_id])
+		end
+		
+	end 
 
 end
