9eb37805c981d4ab6d224a546595c1fc9360d2a7
Tom Holmes <tholmes59@gmail.com>
2019-04-15 20:01:05 +0000

    update users controller with signup and login methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,32 @@
 class UsersController < ApplicationController
   
+  get "/signup" do
+		erb :"users/create_user"
+	end
+	
+	get "/login" do
+		erb :"users/login"
+	end
+
+	post "/users" do
+	  if params[:username] != "" && params[:email] != "" &&   params[:password] != ""
+		   @user = User.create(username: params[:username], email: params[:email], password: params[:password])
+      session[:user_id] = @user.id
+      redirect "/show/#{@user.id}"
+    else
+      redirect "/signup"
+	  end
+  end
+  
+	post "/login" do
+		@user = User.find_by(username: params[:username])
+		
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect "/show/#{@user.id}"
+    else
+      redirect "/"
+    end
+	end
   
 end
