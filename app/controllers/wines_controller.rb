03de674dfaece489eb11cab79a352aa51ed4df21
github username <github email address>
2019-04-16 11:09:12 -0700

    add get and post methods for adding new wine

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -4,5 +4,23 @@ class WinesController < ApplicationController
         @wines = Wine.all 
         erb :"wines/wines"
       end 
+
+    get "/wines/new" do
+      erb :"wines/new"
+    end
+    
+    post "/wines" do
+        if !logged_in?
+            redirect "/users/login"
+        end
+        if params[:wine_name] != ""
+            @wine = Wine.create(wine_name: params[:wine_name], type: params[:type], varietal: params[:varietal], region params[:region], 
+            year: params[:year], price: params[:price], tasting_notes: params[:tasting_notes], user_id: current_user.id)
+            redirect to "/wines/#{@wine.id}"
+        else
+            redirect "/wines/new"
+        end
+    end
   
 end
+
