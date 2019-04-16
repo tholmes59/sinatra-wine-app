69ba7750c2488842275b32aa5cdaf894b644a71f
github username <github email address>
2019-04-16 11:37:13 -0700

    edited post method to correct error

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -14,8 +14,7 @@ class WinesController < ApplicationController
             redirect "/users/login"
         end
         if params[:wine_name] != ""
-            @wine = Wine.create(wine_name: params[:wine_name], type: params[:type], varietal: params[:varietal], region params[:region], 
-            year: params[:year], price: params[:price], tasting_notes: params[:tasting_notes], user_id: current_user.id)
+            @wine = Wine.create(wine_name: params[:wine_name], type: params[:type], varietal: params[:varietal], region: params[:region], year: params[:year], price: params[:price], tasting_notes: params[:tasting_notes], user_id: current_user.id)
             redirect to "/wines/#{@wine.id}"
         else
             redirect "/wines/new"
